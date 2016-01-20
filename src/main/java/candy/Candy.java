931dd567e8963c53c2a987baf4b500c533833ef4
Search 2018 <search2018@gmail.com>
2016-01-19 22:36:01 -0800

    1/19/2016

--- src/main/java/candy/Candy.java
+++ src/main/java/candy/Candy.java
@@ -11,58 +11,26 @@ public class Candy {
         Candy
         https://leetcode.com/problems/candy/
         Difficulty: Hard
-     */
+    */
     public class Solution {
         public int candy(int[] ratings) {
-            int n = ratings.length;
-            int[] candies = new int[n];
-            Arrays.fill(candies, 1);
-            for (int k = 1, i = 1; i < n; i++) {
-                if (ratings[i] > ratings[i - 1]) {
-                    candies[i] = ++k;
-                } else {
-                    k = 1;
-                }
+            int rslt = 0;
+            if (ratings == null || ratings.length == 0) return rslt;
+            int[] candies = new int[ratings.length];
+            for (int i = 0; i < candies.length; i++) {
+                candies[i] = 1;
             }
-            for (int k = 1, i = n - 2; i >= 0; i--) {
-                if (ratings[i] > ratings[i + 1]) {
-                    candies[i] = Math.max(candies[i], ++k);
-                } else {
-                    k = 1;
-                }
+            for (int i = 1; i < candies.length; i++) {
+                if (ratings[i] > ratings[i - 1]) candies[i] = candies[i - 1] + 1;
+                else if (ratings[i] < ratings[i - 1]) candies[i] = 1;
             }
-            int count = 0;
-            for (int i = 0; i < n; i++) {
-                count += candies[i];
+            for (int i = candies.length - 2; i >= 0; i--) {
+                if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) candies[i] = candies[i + 1] + 1;
             }
-            return count;
-        }
-    }
-
-    /*
-        Candy
-        https://leetcode.com/problems/candy/
-        Difficulty: Hard
-    */
-    public class Solution_2 {
-        public int candy(int[] ratings) {
-            int minNum = 0;
-            if (ratings==null || ratings.length==0) return minNum;
-            int[] nums = new int[ratings.length];
-            for (int k=0; k<nums.length; k++) {
-                nums[k] = 1;
+            for (int i = 0; i < candies.length; i++) {
+                rslt += candies[i];
             }
-            for (int i=1; i<nums.length; i++) {
-                if (ratings[i] > ratings[i-1]) nums[i] = nums[i-1] + 1;
-                else if (ratings[i] < ratings[i-1]) nums[i] = 1;
-            }
-            for (int i=nums.length-2; i>=0; i--) {
-                if (ratings[i] > ratings[i+1] && nums[i] <= nums[i+1]) nums[i] = nums[i+1] + 1;
-            }
-            for (int i=0; i<nums.length; i++) {
-                minNum += nums[i];
-            }
-            return minNum;
+            return rslt;
         }
     }
 
--- src/main/java/triangle/Triangle.java
+++ src/main/java/triangle/Triangle.java
@@ -5,23 +5,30 @@ import java.util.Collections;
 import java.util.List;
 
 public class Triangle {
-
+    /*
+        Triangle
+        https://leetcode.com/problems/triangle/
+        Difficulty: Medium
+     */
     public class Solution {
         public int minimumTotal(List<List<Integer>> triangle) {
-            ArrayList<Integer> sum = new ArrayList<Integer>();
-            if (triangle.isEmpty()) {
-                return 0;
-            }
-            sum.add(triangle.get(0).get(0));
-            for (int i = 1; i < triangle.size(); i++) {
-                sum.add(triangle.get(i).get(i) + sum.get(i - 1));
+            if (triangle == null || triangle.size() == 0) return 0;
+            int n = triangle.size();
+            int[] dp = new int[n];
+            dp[0] = triangle.get(0).get(0);
+            for (int i = 1; i < n; i++) {
+                dp[i] = triangle.get(i).get(i) + dp[i - 1];
                 for (int j = i - 1; j >= 1; j--) {
-                    sum.set(j, Math.min(sum.get(j - 1), sum.get(j))
-                            + triangle.get(i).get(j));
+                    dp[j] = Math.min(dp[j-1], dp[j]) +  triangle.get(i).get(j);
                 }
-                sum.set(0, sum.get(0) + triangle.get(i).get(0));
+                dp[0] = dp[0] + triangle.get(i).get(0);
             }
-            return Collections.min(sum);
+
+            int rslt = dp[0];
+            for (int i=1; i<n; i++)
+                if (dp[i] < rslt)
+                    rslt = dp[i];
+            return rslt;
         }
     }
 
