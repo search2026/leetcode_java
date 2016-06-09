3a563b994173fa33be2fcee58b7a28d49eab1832
Daniel Qi <allaboutjst@gmail.com>
2016-06-08 21:10:41 -0700

    some updates

--- src/main/java/graph_valid_tree/GraphValidTree.java
+++ src/main/java/graph_valid_tree/GraphValidTree.java
@@ -7,182 +7,183 @@ import java.util.*;
 import static org.junit.Assert.*;
 
 public class GraphValidTree {
-    /*
-        Graph Valid Tree - DFS
-        Leetcode #261
-        https://leetcode.com/discuss/questions/oj/graph-valid-tree?sort=votes
-        http://buttercola.blogspot.com/2015/08/leetcode-graph-valid-tree.html
-        Difficulty: Medium
-     */
-    public class Solution {
-        private boolean hasCycle(List<List<Integer>> adj, int cur, HashSet<Integer> visited, int parent) {
-            visited.add(cur);
+  /*
+      Graph Valid Tree - DFS
+      Leetcode #261
+      https://leetcode.com/discuss/questions/oj/graph-valid-tree?sort=votes
+      http://buttercola.blogspot.com/2015/08/leetcode-graph-valid-tree.html
+      Difficulty: Medium
+   */
+  public class Solution {
+    private boolean hasCycle(List<HashSet<Integer>> graph, Set<Integer> v, int curr, int prev) {
+      v.add(curr);
 
-            for (int i = 0; i < adj.get(cur).size(); i++) {
-                int neighbor = adj.get(cur).get(i);
-                if ((visited.contains(neighbor) && parent != neighbor) ||
-                        (!visited.contains(neighbor) && hasCycle(adj, neighbor, visited, cur)))
-                    return true;
-            }
+      for (int nbr : graph.get(curr)) {
+        if (v.contains(nbr)) {
+          if (prev != nbr) return true;
+        } else if (hasCycle(graph, v, nbr, curr)) return true;
+      }
 
-            return false;
-        }
-
-        public boolean validTree(int n, int[][] edges) {
-            List<List<Integer>> adj = new ArrayList<List<Integer>>(n);
-            for (int i = 0; i < n; i++)
-                adj.add(i, new ArrayList<Integer>());
-            for (int i = 0; i < edges.length; i++) {
-                int u = edges[i][0], v = edges[i][1];
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-
-            HashSet<Integer> visited = new HashSet<Integer>();
-            if (hasCycle(adj, 0, visited, -1))
-                return false;
-
-            // checks if there is an isolated island
-            return visited.size() == n;
-        }
+      return false;
     }
 
-    /*
-        Graph Valid Tree - BFS
-        Leetcode #261
-        https://leetcode.com/discuss/questions/oj/graph-valid-tree?sort=votes
-        http://buttercola.blogspot.com/2015/08/leetcode-graph-valid-tree.html
-        Difficulty: Medium
-     */
-    public class Solution_2 {
-        public boolean validTree(int n, int[][] edges) {
-            List<Set<Integer>> graph = new ArrayList<Set<Integer>>();
+    public boolean validTree(int n, int[][] edges) {
+      if (edges == null || edges.length == 0 || edges.length != n - 1 ||
+              edges[0] == null || edges[0].length != 2) return false;
 
-            for (int i = 0; i < n; i++) {
-                graph.add(i, new HashSet<Integer>());
-            }
-            for (int[] edge : edges) {
-                graph.get(edge[0]).add(edge[1]);
-                graph.get(edge[1]).add(edge[0]);
-            }
+      List<HashSet<Integer>> graph = new ArrayList<>();
+      for (int i = 0; i < n; i++) graph.add(new HashSet<Integer>());
+      for (int[] edge : edges) {
+        graph.get(edge[0]).add(edge[1]);
+        graph.get(edge[1]).add(edge[0]);
+      }
 
-            HashSet<Integer> visited = new HashSet<Integer>();
-            Queue<Integer> queue = new LinkedList<Integer>();
-            queue.offer(0);
-            while (!queue.isEmpty()) {
-                int cur = queue.poll();
-                if (visited.contains(cur)) return false;
-                visited.add(cur);
-                for (int neighbor : graph.get(cur)) {
-                    if (!visited.contains(neighbor))
-                        queue.offer(neighbor);
-                }
-            }
+      Set<Integer> v = new HashSet<>();
+      if (hasCycle(graph, v, 0, -1)) return false;
 
-            // checks if there is an isolated island
-            return visited.size() == n;
+      // checks if there is an isolated island
+      return v.size() == n;
+    }
+  }
+
+  /*
+      Graph Valid Tree - BFS
+      Leetcode #261
+      https://leetcode.com/discuss/questions/oj/graph-valid-tree?sort=votes
+      http://buttercola.blogspot.com/2015/08/leetcode-graph-valid-tree.html
+      Difficulty: Medium
+   */
+  public class Solution_2 {
+    public boolean validTree(int n, int[][] edges) {
+      if (edges == null || edges.length == 0 || edges.length != n - 1 ||
+              edges[0] == null || edges[0].length != 2) return false;
+
+      List<HashSet<Integer>> graph = new ArrayList<>();
+      for (int i = 0; i < n; i++) graph.add(new HashSet<Integer>());
+      for (int[] edge : edges) {
+        graph.get(edge[0]).add(edge[1]);
+        graph.get(edge[1]).add(edge[0]);
+      }
+
+      Queue<Integer> q = new ArrayDeque<>();
+      Set<Integer> v = new HashSet<>();
+      q.offer(0);
+      while (!q.isEmpty()) {
+        int curr = q.poll();
+        if (v.contains(curr)) return false;
+        v.add(curr);
+
+        for (int nbr : graph.get(curr)) {
+          if (!v.contains(nbr)) q.offer(nbr);
         }
+      }
+
+      // checks if there is an isolated island
+      return v.size() == n;
+    }
+  }
+
+  /*
+      Graph Valid Tree - Union & Find
+      Leetcode #261
+      https://leetcode.com/discuss/questions/oj/graph-valid-tree?sort=votes
+      http://buttercola.blogspot.com/2015/08/leetcode-graph-valid-tree.html
+      Difficulty: Medium
+  */
+  public class Solution_3 {
+    private int findRoot(int[] root, int n) {
+      while (root[n] != n) n = root[n];
+      return n;
     }
 
-    /*
-        Graph Valid Tree - Union & Find
-        Leetcode #261
-        https://leetcode.com/discuss/questions/oj/graph-valid-tree?sort=votes
-        http://buttercola.blogspot.com/2015/08/leetcode-graph-valid-tree.html
-        Difficulty: Medium
-    */
-    public class Solution_3 {
-        public boolean validTree(int n, int[][] edges) {
-            int[] root = new int[n];
-            for (int i=0; i<n;i++)
-                root[i] = i;
+    public boolean validTree(int n, int[][] edges) {
+      if (edges == null || edges.length == 0 || edges.length != n - 1 ||
+              edges[0] == null || edges[0].length != 2) return false;
 
-            for (int[] edge : edges) {
-                int x = findRoot(root, edge[0]);
-                int y = findRoot(root, edge[1]);
-                // if two vertices are in the same set, there's a cycle
-                if (x == y) return false;
-                // union
-                root[x] = y;
-            }
+      int[] root = new int[n];
+      for (int i = 0; i < n; i++) root[i] = i;
 
-            return edges.length == n - 1;
-        }
+      for (int[] edge : edges) {
+        int x = findRoot(root, edge[0]);
+        int y = findRoot(root, edge[1]);
 
-        public int findRoot(int root[], int n) {
-            while (root[n] != n) n = root[n];
-            return n;
-        }
+        if (x == y) return false;
+        // union
+        root[x] = y;
+      }
+
+      return true;
+    }
+  }
+
+  public static class UnitTest {
+    @Test
+    public void test1() {
+      Solution sol = new GraphValidTree().new Solution();
+      int n = 5;
+      int[][] edges = new int[][]{
+              new int[]{0, 1},
+              new int[]{0, 2},
+              new int[]{0, 3},
+              new int[]{1, 4}
+      };
+      assertTrue(sol.validTree(n, edges));
+
+      n = 5;
+      edges = new int[][]{
+              new int[]{0, 1},
+              new int[]{1, 2},
+              new int[]{2, 3},
+              new int[]{1, 3},
+              new int[]{1, 4}
+      };
+      assertFalse(sol.validTree(n, edges));
     }
 
-    public static class UnitTest {
-        @Test
-        public void test1() {
-            Solution sol = new GraphValidTree().new Solution();
-            int n = 5;
-            int[][] edges = new int[][]{
-                    new int[]{0, 1},
-                    new int[]{0, 2},
-                    new int[]{0, 3},
-                    new int[]{1, 4}
-            };
-            assertTrue(sol.validTree(n, edges));
+    @Test
+    public void test2() {
+      Solution_2 sol = new GraphValidTree().new Solution_2();
+      int n = 5;
+      int[][] edges = new int[][]{
+              new int[]{0, 1},
+              new int[]{0, 2},
+              new int[]{0, 3},
+              new int[]{1, 4}
+      };
+      assertTrue(sol.validTree(n, edges));
 
-            n = 5;
-            edges = new int[][]{
-                    new int[]{0, 1},
-                    new int[]{1, 2},
-                    new int[]{2, 3},
-                    new int[]{1, 3},
-                    new int[]{1, 4}
-            };
-            assertFalse(sol.validTree(n, edges));
-        }
-
-        @Test
-        public void test2() {
-            Solution_2 sol = new GraphValidTree().new Solution_2();
-            int n = 5;
-            int[][] edges = new int[][]{
-                    new int[]{0, 1},
-                    new int[]{0, 2},
-                    new int[]{0, 3},
-                    new int[]{1, 4}
-            };
-            assertTrue(sol.validTree(n, edges));
-
-            n = 5;
-            edges = new int[][]{
-                    new int[]{0, 1},
-                    new int[]{1, 2},
-                    new int[]{2, 3},
-                    new int[]{1, 3},
-                    new int[]{1, 4}
-            };
-            assertFalse(sol.validTree(n, edges));
-        }
-
-        @Test
-        public void test3() {
-            Solution_3 sol = new GraphValidTree().new Solution_3();
-            int n = 5;
-            int[][] edges = new int[][]{
-                    new int[]{0, 1},
-                    new int[]{0, 2},
-                    new int[]{0, 3},
-                    new int[]{1, 4}
-            };
-            assertTrue(sol.validTree(n, edges));
-
-            n = 5;
-            edges = new int[][]{
-                    new int[]{0, 1},
-                    new int[]{1, 2},
-                    new int[]{2, 3},
-                    new int[]{1, 3},
-                    new int[]{1, 4}
-            };
-            assertFalse(sol.validTree(n, edges));
-        }
+      n = 5;
+      edges = new int[][]{
+              new int[]{0, 1},
+              new int[]{1, 2},
+              new int[]{2, 3},
+              new int[]{1, 3},
+              new int[]{1, 4}
+      };
+      assertFalse(sol.validTree(n, edges));
     }
+
+    @Test
+    public void test3() {
+      Solution_3 sol = new GraphValidTree().new Solution_3();
+      int n = 5;
+      int[][] edges = new int[][]{
+              new int[]{0, 1},
+              new int[]{0, 2},
+              new int[]{0, 3},
+              new int[]{1, 4}
+      };
+      assertTrue(sol.validTree(n, edges));
+
+      n = 5;
+      edges = new int[][]{
+              new int[]{0, 1},
+              new int[]{1, 2},
+              new int[]{2, 3},
+              new int[]{1, 3},
+              new int[]{1, 4}
+      };
+      assertFalse(sol.validTree(n, edges));
+    }
+  }
 }
--- src/main/java/two_sum/TwoSum.java
+++ src/main/java/two_sum/TwoSum.java
@@ -7,96 +7,119 @@ import java.util.*;
 import static org.junit.Assert.*;
 
 public class TwoSum {
-    /*
-        Two Sum
-        Leetcode #1
-        https://leetcode.com/problems/two-sum/
-        Difficulty: Medium
-     */
-    public class Solution {
-        public int[] twoSum(int[] nums, int target) {
-            HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-            map.put(nums[0], 1);
-            for (int i = 1; i < nums.length; ++i) {
-                int diff = target - nums[i];
-                if (map.containsKey(diff)) {
-                    return new int[]{map.get(diff), i + 1};
-                } else map.put(nums[i], i + 1);
-            }
-            return null;
+  /*
+      Two Sum - HashMap
+      Leetcode #1
+      https://leetcode.com/problems/two-sum/
+      Difficulty: Medium
+   */
+  public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      if (nums == null || nums.length < 2) return null;
+
+      int n = nums.length;
+      Map<Integer, Integer> map = new HashMap<>();
+      map.put(nums[0], 1);
+      for (int i=1; i< n; i++) {
+        int diff = target - nums[i];
+        if (map.containsKey(diff)) {
+          return new int[]{map.get(diff), i+1};
+        } else {
+          map.put(nums[i], i+1);
         }
+      }
+
+      return null;
+    }
+  }
+
+  /*
+      Two Sum II - Two Pointers
+      Leetcode #167
+      https://leetcode.com/discuss/oj/two-sum-ii-input-array-is-sorted
+      http://www.cnblogs.com/ganganloveu/p/4198968.html
+      Difficulty: Medium
+   */
+  public class Solution_2 {
+    public int[] twoSum(int[] nums, int target) {
+      if (nums == null || nums.length < 2) return null;
+
+      int p1=0, p2=nums.length-1;
+
+      while(p1<p2) {
+        int sum = nums[p1] + nums[p2];
+        if (sum == target) return new int[]{p1+1, p2+1};
+        else if (sum < target) p1++;
+        else p2--;
+      }
+
+      return null;
+    }
+  }
+
+  /*
+      Two Sum III Data Structure
+      Leetcode #170
+      https://leetcode.com/discuss/questions/oj/two-sum-iii-data-structure-design?sort=votes
+      http://www.cnblogs.com/EdwardLiu/p/4252598.html
+      Difficulty: Medium
+   */
+  public class TwoSumDS {
+    private Map<Integer, Integer> map;
+
+    TwoSumDS() {
+      map = new HashMap<Integer, Integer>();
     }
 
-    /*
-        Two Sum II
-        Leetcode #167
-        https://leetcode.com/discuss/oj/two-sum-ii-input-array-is-sorted
-        http://www.cnblogs.com/ganganloveu/p/4198968.html
-        Difficulty: Medium
-     */
-    public class Solution_2 {
-        public int[] twoSumII(final int[] nums, int target) {
-            if (nums == null || nums.length == 0)
-                return null;
-
-            int i = 0;
-            int j = nums.length - 1;
-
-            while (i < j) {
-                int x = nums[i] + nums[j];
-                if (x < target) {
-                    ++i;
-                } else if (x > target) {
-                    j--;
-                } else {
-                    return new int[]{i + 1, j + 1};
-                }
-            }
-
-            return null;
-        }
+    public void add(int number) {
+      if (map.containsKey(number)) {
+        map.put(number, map.get(number) + 1);
+      } else {
+        map.put(number, 1);
+      }
     }
 
-    /*
-        Two Sum III Data Structure
-        Leetcode #170
-        https://leetcode.com/discuss/questions/oj/two-sum-iii-data-structure-design?sort=votes
-        http://www.cnblogs.com/EdwardLiu/p/4252598.html
-        Difficulty: Medium
-     */
-    public class Solution_3 {
-        private HashMap<Integer, Integer> elements = new HashMap<Integer, Integer>();
+    public boolean find(int target) {
+      if (map.size() < 2) return false;
 
-        public void add(int number) {
-            if (elements.containsKey(number)) {
-                elements.put(number, elements.get(number) + 1);
-            } else {
-                elements.put(number, 1);
-            }
+      for (Integer key : map.keySet()){
+        int diff = target - key;
+        if (map.containsKey(diff)) {
+          if (diff != key) return true;
+          else if (map.get(key) > 1) return true;
         }
+      }
 
-        public boolean find(int value) {
-            for (Integer i : elements.keySet()) {
-                int target = value - i;
-                if (elements.containsKey(target)) {
-                    if (i == target && elements.get(target) < 2) {
-                        continue;
-                    }
-                    return true;
-                }
-            }
-            return false;
-        }
+      return false;
+    }
+  }
+
+
+  public static class UnitTest {
+    @Test
+    public void test1() {
+      Solution sol = new TwoSum().new Solution();
+      int[] test = {5, 75, 25};
+      assertArrayEquals(new int[]{2, 3}, sol.twoSum(test, 100));
+      assertArrayEquals(null, sol.twoSum(test, 99));
     }
 
-    public static class UnitTest {
-        @Test
-        public void test1() {
-            new TwoSum().new Solution().twoSum(new int[]{5, 75, 25}, 100);
-        }
-
-        public void test2() {
-            new TwoSum().new Solution().twoSum(new int[]{5, 75, 25}, 100);
-        }
+    @Test
+    public void test2() {
+      Solution_2 sol = new TwoSum().new Solution_2();
+      int[] test = {5, 25, 75};
+      assertArrayEquals(new int[]{2, 3}, sol.twoSum(test, 100));
+      assertArrayEquals(null, sol.twoSum(test, 99));
     }
+
+    @Test
+    public void test3() {
+      TwoSumDS sol = new TwoSum().new TwoSumDS();
+      sol.add(5);
+      sol.add(75);
+      sol.add(25);
+      assertTrue(sol.find(100));
+      assertFalse(sol.find(99));
+    }
+  }
 }
