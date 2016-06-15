f0343baf3a5bc946f7cab9e55f7ff666a7213d61
search2018 <search2018@gmail.com>
2016-06-14 23:58:19 -0700

    up to #358

---
+++ src/main/java/count_numbers_with_unique_digits/CountNumberswithUniqueDigits.java
@@ -0,0 +1,98 @@
+package count_numbers_with_unique_digits;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CountNumberswithUniqueDigits {
+  /*
+      Count Numbers with Unique Digits - Dynamic Programming
+      Leetcode #357
+      https://leetcode.com/problems/count-numbers-with-unique-digits/
+      Difficulty: Medium
+   */
+  public class Solution {
+    private int search(long prev, long max, boolean[] visited) {
+      int count = 0;
+      if (prev < max) {
+        count += 1;
+      } else {
+        return count;
+      }
+
+      for (int i = 0; i < 10; i++) {
+        if (!visited[i]) {
+          visited[i] = true;
+          long cur = 10 * prev + i;
+          count += search(cur, max, visited);
+          visited[i] = false;
+        }
+      }
+
+      return count;
+    }
+
+    public int countNumbersWithUniqueDigits(int n) {
+      if (n == 0) return 1;
+      if (n > 10) return countNumbersWithUniqueDigits(10);
+
+      int count = 1; // x == 0
+      long max = (long) Math.pow(10, n);
+      boolean[] visited = new boolean[10];
+
+      for (int i = 1; i < 10; i++) {
+        visited[i] = true;
+        count += search(i, max, visited);
+        visited[i] = false;
+      }
+
+      return count;
+    }
+  }
+
+  /*
+      Count Numbers with Unique Digits - Dynamic Programming
+      Leetcode #357
+      https://leetcode.com/problems/count-numbers-with-unique-digits/
+      Difficulty: Medium
+   */
+  public class Solution_2 {
+    public int countNumbersWithUniqueDigits(int n) {
+      if (n == 0) return 1;
+      if (n > 10) n = 10;
+
+      int count = 10;
+      int uniqueDigits = 9;
+      int available = 9;
+      while (n-- > 1 && available > 0) {
+        uniqueDigits = uniqueDigits * available;
+        count += uniqueDigits;
+        available--;
+      }
+      return count;
+    }
+  }
+
+  public static class UnitTest {
+    @Test
+    public void test1() {
+      Solution sol = new CountNumberswithUniqueDigits().new Solution();
+      assertEquals(1, sol.countNumbersWithUniqueDigits(0));
+      assertEquals(10, sol.countNumbersWithUniqueDigits(1));
+      assertEquals(91, sol.countNumbersWithUniqueDigits(2));
+      assertEquals(739, sol.countNumbersWithUniqueDigits(3));
+      assertEquals(5275, sol.countNumbersWithUniqueDigits(4));
+    }
+
+    @Test
+    public void test2() {
+      Solution_2 sol = new CountNumberswithUniqueDigits().new Solution_2();
+      assertEquals(1, sol.countNumbersWithUniqueDigits(0));
+      assertEquals(10, sol.countNumbersWithUniqueDigits(1));
+      assertEquals(91, sol.countNumbersWithUniqueDigits(2));
+      assertEquals(739, sol.countNumbersWithUniqueDigits(3));
+      assertEquals(5275, sol.countNumbersWithUniqueDigits(4));
+    }
+  }
+}
+
