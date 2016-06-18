cd523e83962503dc0ced167c55a275bb2d1ed4c8
search2018 <search2018@gmail.com>
2016-06-17 23:04:36 -0700

    Updated 4 questions

--- src/main/java/largest_bst_subtree/LargestBSTSubtree.java
+++ src/main/java/largest_bst_subtree/LargestBSTSubtree.java
@@ -6,99 +6,164 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class LargestBSTSubtree {
-    /*
-        Largest BST Subtree - New Node Structure
-        http://www.cnblogs.com/Dylan-Java-NYC/p/5187436.html
-        Difficulty: Medium
-     */
-    public class Solution {
-        private Node search(TreeNode root, int[] rslt) {
-            Node cur = new Node();
-            if (root == null) {
-                cur.isBST = true;
-                return cur;
-            }
-            Node left = search(root.left, rslt);
-            Node right = search(root.right, rslt);
-            if (left.isBST && root.val > left.max && right.isBST && root.val < right.min) {
-                cur.isBST = true;
-                cur.min = Math.min(root.val, left.min);
-                cur.max = Math.max(root.val, right.max);
-                cur.size = left.size + right.size + 1;
-                if (cur.size > rslt[0]) {
-                    rslt[0] = cur.size;
-                }
-            }
-            return cur;
+  /*
+      Largest BST Subtree - New Node Structure
+      Leetcode #333
+      https://leetcode.com/discuss/questions/oj/largest-bst-subtree?sort=votes
+      http://www.cnblogs.com/grandyang/p/5188938.html
+      Difficulty: Medium
+   */
+  public class Solution {
+    private Node search(TreeNode root, int[] count) {
+      Node cur = new Node();
+      if (root == null) {
+        cur.isBST = true;
+        return cur;
+      }
+      Node left = search(root.left, count);
+      Node right = search(root.right, count);
+      if (left.isBST && root.val > left.max && right.isBST && root.val < right.min) {
+        cur.isBST = true;
+        cur.min = Math.min(root.val, left.min);
+        cur.max = Math.max(root.val, right.max);
+        cur.size = left.size + right.size + 1;
+        if (cur.size > count[0]) {
+          count[0] = cur.size;
         }
-
-        public int largestBSTSubtree(TreeNode root) {
-            int[] rslt = {0};
-            search(root, rslt);
-            return rslt[0];
-        }
+      }
+      return cur;
     }
 
-    class Node {
-        boolean isBST;
-        int min;
-        int max;
-        int size;
+    public int largestBSTSubtree(TreeNode root) {
+      int[] count = {0};
+      search(root, count);
+      return count[0];
+    }
+  }
 
-        public Node() {
-            isBST = false;
-            min = Integer.MAX_VALUE;
-            max = Integer.MIN_VALUE;
-            size = 0;
-        }
+  class Node {
+    boolean isBST;
+    int min;
+    int max;
+    int size;
+
+    public Node() {
+      isBST = false;
+      min = Integer.MAX_VALUE;
+      max = Integer.MIN_VALUE;
+      size = 0;
+    }
+  }
+
+  /*
+      Largest BST Subtree - O(NlogN)
+      Leetcode #333
+      https://leetcode.com/discuss/questions/oj/largest-bst-subtree?sort=votes
+      http://www.cnblogs.com/grandyang/p/5188938.html
+      Difficulty: Medium
+   */
+  public class Solution_2 {
+    private int isValidBST(TreeNode node, int min, int max) {
+      if (node == null) {
+        return 0;
+      }
+      if (node.val < min || node.val > max) {
+        return -1;
+      }
+      int left = isValidBST(node.left, min, node.val);
+      int right = isValidBST(node.right, node.val, max);
+      return (left >= 0 && right >= 0) ? left + right + 1 : -1;
     }
 
+    public int largestBSTSubtree(TreeNode root) {
+      int count = isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+      if (count >= 0) return count;
 
-    /*
-        Largest BST Subtree
-        http://codingmelon.com/2016/02/13/largest-bst-subtree-leetcode-333/
-        Difficulty: Medium
-     */
-    public class Solution_2 {
-        private boolean search(TreeNode root, int[] min, int[] max, int[] rslt) {
-            if (root == null) return true;
-
-            int[] lMin = {Integer.MAX_VALUE};
-            int[] lMax = {Integer.MIN_VALUE};
-            int[] lSize = {0};
-            boolean isLeftValid = search(root.left, lMin, lMax, lSize);
-
-            int[] rMin = {Integer.MAX_VALUE};
-            int[] rMax = {Integer.MIN_VALUE};
-            int[] rSize = {0};
-            boolean isRightValid = search(root.right, rMin, rMax, rSize);
-
-            if (!isLeftValid || !isRightValid || root.val <= lMax[0] || root.val >= rMin[0]) {
-                //search[0] = Math.max(lSize[0], rSize[0]);
-                return false;
-            }
-
-            min[0] = Math.min(lMin[0], root.val);
-            max[0] = Math.max(rMax[0], root.val);
-            rslt[0] = lSize[0] + rSize[0] + 1;
-
-            return true;
-        }
-
-        public int largestBSTSubtree(TreeNode root) {
-            int[] rslt = {0};
-            int[] min = {0};
-            int[] max = {0};
-            search(root, min, max, rslt);
-            return rslt[0];
-        }
+      return Math.max(largestBSTSubtree(root.left), largestBSTSubtree(root.right));
     }
-    public static class UnitTest {
-        @Test
-        public void test1() {
-            Solution sol = new LargestBSTSubtree().new Solution();
-            assertEquals(1, 1);
-        }
+  }
+
+  /*
+      Largest BST Subtree - O(N)
+      Leetcode #333
+      https://leetcode.com/discuss/questions/oj/largest-bst-subtree?sort=votes
+      http://www.cnblogs.com/grandyang/p/5188938.html
+      Difficulty: Medium
+   */
+  public class Solution_3 {
+    private boolean search(TreeNode root, int[] min, int[] max, int[] count) {
+      if (root == null) return true;
+
+      int[] lMin = {Integer.MAX_VALUE};
+      int[] lMax = {Integer.MIN_VALUE};
+      int[] lSize = {0};
+      boolean isLeftValid = search(root.left, lMin, lMax, lSize);
+
+      int[] rMin = {Integer.MAX_VALUE};
+      int[] rMax = {Integer.MIN_VALUE};
+      int[] rSize = {0};
+      boolean isRightValid = search(root.right, rMin, rMax, rSize);
+
+      if (!isLeftValid || !isRightValid || (root.left != null && root.val <= lMax[0]) || (root.right != null && root.val >= rMin[0])) {
+        count[0] = Math.max(lSize[0], rSize[0]);
+        return false;
+      }
+
+      min[0] = root.left == null ? root.val : lMin[0];
+      max[0] = root.right == null ? root.val : rMax[0];
+      count[0] = lSize[0] + rSize[0] + 1;
+
+      return true;
     }
+
+    public int largestBSTSubtree(TreeNode root) {
+      int[] count = {0};
+      int[] min = {Integer.MIN_VALUE};
+      int[] max = {Integer.MAX_VALUE};
+      search(root, min, max, count);
+      return count[0];
+    }
+  }
+
+  public static class UnitTest {
+    @Test
+    public void test1() {
+      Solution sol = new LargestBSTSubtree().new Solution();
+      TreeNode root = new TreeNode(10);
+      root.left = new TreeNode(5);
+      root.right = new TreeNode(15);
+      root.left.left = new TreeNode(1);
+      root.left.right = new TreeNode(8);
+      root.right.left = null;
+      root.right.right = new TreeNode(7);
+      assertEquals(3, sol.largestBSTSubtree(root));
+    }
+
+    @Test
+    public void test2() {
+      Solution_2 sol = new LargestBSTSubtree().new Solution_2();
+      TreeNode root = new TreeNode(10);
+      root.left = new TreeNode(5);
+      root.right = new TreeNode(15);
+      root.left.left = new TreeNode(1);
+      root.left.right = new TreeNode(8);
+      root.right.left = null;
+      root.right.right = new TreeNode(7);
+      assertEquals(3, sol.largestBSTSubtree(root));
+    }
+
+    @Test
+    public void test3() {
+      Solution_3 sol = new LargestBSTSubtree().new Solution_3();
+      TreeNode root = new TreeNode(10);
+      root.left = new TreeNode(5);
+      root.right = new TreeNode(15);
+      root.left.left = new TreeNode(1);
+      root.left.right = new TreeNode(8);
+      root.right.left = null;
+      root.right.right = new TreeNode(7);
+      assertEquals(3, sol.largestBSTSubtree(root));
+    }
+  }
 }
 
--- src/main/java/sort_transformed_array/SortTransformedArray.java
+++ src/main/java/sort_transformed_array/SortTransformedArray.java
@@ -4,30 +4,56 @@ import org.junit.Test;
 
 import java.util.PriorityQueue;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class SortTransformedArray {
   /*
-      Sort Transformed Array
+      Sort Transformed Array - Heap
       Leetcode #360
       https://leetcode.com/discuss/oj/sort-transformed-array
       Difficulty: Medium
    */
   public class Solution {
     public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
-      PriorityQueue<Integer> q = new PriorityQueue<Integer>();
+      if (nums == null || nums.length == 0) return new int[]{};
+      PriorityQueue<Integer> q = new PriorityQueue<>();
       for (int i = 0; i < nums.length; i++) {
-        int temp = value(nums[i], a, b, c);
+        int temp = a * nums[i] * nums[i] + b * nums[i] + c;
         q.offer(temp);
       }
+
       int[] rslt = new int[nums.length];
       for (int i = 0; i < nums.length; i++) rslt[i] = q.poll();
 
       return rslt;
     }
+  }
 
-    private int value(int x, int a, int b, int c) {
+  /*
+      Sort Transformed Array - Two Pointers
+      Leetcode #360
+      https://leetcode.com/discuss/oj/sort-transformed-array
+      Difficulty: Medium
+   */
+  public class Solution_2 {
+    private int cal(int x, int a, int b, int c) {
       return a * x * x + b * x + c;
+    }
+
+    public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
+      if (nums == null || nums.length == 0) return new int[]{};
+      int n = nums.length, p1 = 0, p2 = n - 1;
+      int[] rslt = new int[n];
+      int idx = a >= 0 ? n - 1 : 0;
+      while (p1 <= p2) {
+        if (a >= 0) {
+          rslt[idx--] = cal(nums[p1], a, b, c) >= cal(nums[p2], a, b, c) ? cal(nums[p1++], a, b, c) : cal(nums[p2--], a, b, c);
+        } else {
+          rslt[idx++] = cal(nums[p1], a, b, c) >= cal(nums[p2], a, b, c) ? cal(nums[p2--], a, b, c) : cal(nums[p1++], a, b, c);
+        }
+      }
+      return rslt;
     }
   }
 
@@ -35,7 +61,21 @@ public class SortTransformedArray {
     @Test
     public void test1() {
       Solution sol = new SortTransformedArray().new Solution();
-      assertEquals(1, 1);
+      int[] testArr = {-4, -2, 2, 4};
+      int[] rslt1 = {3, 9, 15, 33};
+      int[] rslt2 = {-23, -5, 1, 7};
+      assertArrayEquals(rslt1, sol.sortTransformedArray(testArr, 1, 3, 5));
+      assertArrayEquals(rslt2, sol.sortTransformedArray(testArr, -1, 3, 5));
+    }
+
+    @Test
+    public void test2() {
+      Solution_2 sol = new SortTransformedArray().new Solution_2();
+      int[] testArr = {-4, -2, 2, 4};
+      int[] rslt1 = {3, 9, 15, 33};
+      int[] rslt2 = {-23, -5, 1, 7};
+      assertArrayEquals(rslt1, sol.sortTransformedArray(testArr, 1, 3, 5));
+      assertArrayEquals(rslt2, sol.sortTransformedArray(testArr, -1, 3, 5));
     }
   }
 }
