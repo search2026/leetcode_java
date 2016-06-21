7a9d980eff3c4247dd80fb5258d26fdc20dfc319
search2018 <search2018@gmail.com>
2016-06-20 22:55:40 -0700

    1. add leetcode #362 2. update 7 questions

--- src/main/java/insert_interval/InsertInterval.java
+++ src/main/java/insert_interval/InsertInterval.java
@@ -4,53 +4,120 @@ import common.Interval;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class InsertInterval {
-    /*
-        Insert Interval
-        Leetcode #57
-        https://leetcode.com/problems/insert-interval/
-        Difficulty: Hard
-     */
-    public class Solution {
-        public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-            if (newInterval == null || intervals == null) return intervals;
+  /*
+      Insert Interval
+      Leetcode #57
+      https://leetcode.com/problems/insert-interval/
+      Difficulty: Hard
+   */
+  public class Solution {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+      if (newInterval == null || intervals == null) return intervals;
 
-            List<Interval> rslt = new ArrayList<Interval>();
-            int insertPos = 0;
+      List<Interval> rslt = new ArrayList<>();
+      int pos = 0;
 
-            for (Interval interval : intervals) {
-                if (interval.end < newInterval.start) {
-                    rslt.add(interval);
-                    insertPos++;
-                } else if (interval.start > newInterval.end) {
-                    rslt.add(interval);
-                } else {
-                    newInterval.start = Math.min(interval.start, newInterval.start);
-                    newInterval.end = Math.max(interval.end, newInterval.end);
-                }
-            }
-
-            rslt.add(insertPos, newInterval);
-
-            return rslt;
+      for (Interval interval : intervals) {
+        if (interval.end < newInterval.start) {
+          rslt.add(interval);
+          pos++;
+        } else if (interval.start > newInterval.end) {
+          rslt.add(interval);
+        } else {
+          newInterval.start = Math.min(interval.start, newInterval.start);
+          newInterval.end = Math.max(interval.end, newInterval.end);
         }
+      }
+
+      rslt.add(pos, newInterval);
+
+      return rslt;
+    }
+  }
+
+  /*
+      Insert Interval
+      Leetcode #57
+      https://leetcode.com/problems/insert-interval/
+      Difficulty: Hard
+   */
+  public class Solution_2 {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+      if (newInterval == null || intervals == null) return intervals;
+
+      int pos = 0;
+      while (pos < intervals.size() && intervals.get(pos).end < newInterval.start) pos++;
+      while (pos < intervals.size() && intervals.get(pos).start <= newInterval.end) {
+        newInterval = new Interval(Math.min(intervals.get(pos).start, newInterval.start), Math.max(intervals.get(pos).end, newInterval.end));
+        intervals.remove(pos);
+      }
+      intervals.add(pos, newInterval);
+      return intervals;
+    }
+  }
+
+  public static class UnitTest {
+    @Test
+    public void test1() {
+      Solution sol = new InsertInterval().new Solution();
+      List<Interval> intervals = new ArrayList<>();
+      intervals.add(new Interval(1, 3));
+      intervals.add(new Interval(6, 9));
+      List<Interval> rslt = sol.insert(intervals, new Interval(2, 5));
+      assertEquals(2, rslt.size());
+      assertEquals(1, rslt.get(0).start);
+      assertEquals(5, rslt.get(0).end);
+      assertEquals(6, rslt.get(1).start);
+      assertEquals(9, rslt.get(1).end);
+
+      intervals = new ArrayList<>();
+      intervals.add(new Interval(1, 2));
+      intervals.add(new Interval(3, 5));
+      intervals.add(new Interval(6, 7));
+      intervals.add(new Interval(8, 10));
+      intervals.add(new Interval(12, 16));
+      rslt = sol.insert(intervals, new Interval(4, 9));
+      assertEquals(3, rslt.size());
+      assertEquals(1, rslt.get(0).start);
+      assertEquals(2, rslt.get(0).end);
+      assertEquals(3, rslt.get(1).start);
+      assertEquals(10, rslt.get(1).end);
+      assertEquals(12, rslt.get(2).start);
+      assertEquals(16, rslt.get(2).end);
     }
 
-    public static class UnitTest {
-        @Test
-        public void testInsert() {
-            Solution sol = new InsertInterval().new Solution();
-            List<Interval> intervals = new ArrayList<Interval>();
-            intervals.add(new Interval(1, 3));
-            intervals.add(new Interval(6, 9));
-            List<Interval> rslt = sol.insert(intervals, new Interval(2,5));
-            assertEquals(1, rslt.get(0).start);
-            assertEquals(5, rslt.get(0).end);
-        }
+    @Test
+    public void test2() {
+      Solution_2 sol = new InsertInterval().new Solution_2();
+      List<Interval> intervals = new ArrayList<>();
+      intervals.add(new Interval(1, 3));
+      intervals.add(new Interval(6, 9));
+      List<Interval> rslt = sol.insert(intervals, new Interval(2, 5));
+      assertEquals(2, rslt.size());
+      assertEquals(1, rslt.get(0).start);
+      assertEquals(5, rslt.get(0).end);
+      assertEquals(6, rslt.get(1).start);
+      assertEquals(9, rslt.get(1).end);
+
+      intervals = new ArrayList<>();
+      intervals.add(new Interval(1, 2));
+      intervals.add(new Interval(3, 5));
+      intervals.add(new Interval(6, 7));
+      intervals.add(new Interval(8, 10));
+      intervals.add(new Interval(12, 16));
+      rslt = sol.insert(intervals, new Interval(4, 9));
+      assertEquals(3, rslt.size());
+      assertEquals(1, rslt.get(0).start);
+      assertEquals(2, rslt.get(0).end);
+      assertEquals(3, rslt.get(1).start);
+      assertEquals(10, rslt.get(1).end);
+      assertEquals(12, rslt.get(2).start);
+      assertEquals(16, rslt.get(2).end);
     }
+  }
 }
--- src/main/java/sort_list/SortList.java
+++ src/main/java/sort_list/SortList.java
@@ -3,66 +3,74 @@ package sort_list;
 import common.ListNode;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class SortList {
-    /*
-        Sort List
-        Leetcode #148
-        https://leetcode.com/problems/sort-list/
-        Difficulty: Medium
-     */
-    public class Solution {
-        public ListNode merge(ListNode head1, ListNode head2) {
-            if (head1 == null) return head2;
-            if (head2 == null) return head1;
-            ListNode dummy = new ListNode(-1);
-            ListNode cur = dummy;
+  /*
+      Sort List
+      Leetcode #148
+      https://leetcode.com/problems/sort-list/
+      Difficulty: Medium
+   */
+  public class Solution {
+    private ListNode merge(ListNode head1, ListNode head2) {
+      if (head1 == null) return head2;
+      if (head2 == null) return head1;
+      ListNode dummy = new ListNode(-1);
+      ListNode curr = dummy;
 
-            while (head1 != null && head2 != null) {
-                if (head1.val <= head2.val) {
-                    cur.next = head1;
-                    head1 = head1.next;
-                } else {
-                    cur.next = head2;
-                    head2 = head2.next;
-                }
-                cur = cur.next;
-            }
-
-            if (head1 == null) cur.next = head2;
-            if (head2 == null) cur.next = head1;
-
-            return dummy.next;
+      while (head1 != null && head2 != null) {
+        if (head1.val <= head2.val) {
+          curr.next = head1;
+          head1 = head1.next;
+        } else {
+          curr.next = head2;
+          head2 = head2.next;
         }
+        curr = curr.next;
+      }
 
-        public ListNode mergesort(ListNode head) {
-            if (head == null || head.next == null) return head;
-            ListNode dummy = new ListNode(-1);
-            dummy.next = head;
-            ListNode slow = dummy;
-            ListNode fast = dummy;
-            while (fast != null && fast.next != null) {
-                fast = fast.next.next;
-                slow = slow.next;
-            }
-            ListNode head1 = dummy.next;
-            ListNode head2 = slow.next;
-            slow.next = null;
-            return merge(mergesort(head1), mergesort(head2));
-        }
+      if (head1 == null) curr.next = head2;
+      if (head2 == null) curr.next = head1;
 
-        public ListNode sortList(ListNode head) {
-            if (head == null || head.next == null) return head;
-            return mergesort(head);
-        }
+      return dummy.next;
     }
 
-    public static class UnitTest {
-        @Test
-        public void test1() {
-            Solution sol = new SortList().new Solution();
-            assertTrue(true);
-        }
+    private ListNode mergesort(ListNode head) {
+      if (head == null || head.next == null) return head;
+      ListNode dummy = new ListNode(-1);
+      dummy.next = head;
+      ListNode slow = dummy;
+      ListNode fast = dummy;
+      while (fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+      }
+      ListNode head1 = dummy.next;
+      ListNode head2 = slow.next;
+      slow.next = null;
+      return merge(mergesort(head1), mergesort(head2));
     }
+
+    public ListNode sortList(ListNode head) {
+      if (head == null || head.next == null) return head;
+      return mergesort(head);
+    }
+  }
+
+  public static class UnitTest {
+    @Test
+    public void test1() {
+      Solution sol = new SortList().new Solution();
+      ListNode head = new ListNode(3);
+      head.next = new ListNode(5);
+      head.next.next = new ListNode(-1);
+      ListNode sorted = sol.sortList(head);
+      assertEquals(-1, sorted.val);
+      assertEquals(3, sorted.next.val);
+      assertEquals(5, sorted.next.next.val);
+      assertNull(sorted.next.next.next);
+    }
+  }
 }
