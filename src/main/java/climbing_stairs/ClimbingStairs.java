183e05db4c896308f1ff30b5db6398d4c6b7b53e
Daniel <search2018@gmail.com>
2016-06-24 23:01:24 -0700

    update 5 questions

--- src/main/java/climbing_stairs/ClimbingStairs.java
+++ src/main/java/climbing_stairs/ClimbingStairs.java
@@ -4,46 +4,66 @@ import org.junit.Test;
 
 import java.lang.Math;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class ClimbingStairs {
-    /*
-        Climbing Stairs
-        Leetcode #70
-        https://leetcode.com/problems/climbing-stairs/
-        Difficulty: Easy
-     */
-    public class Solution {
-        public int climbStairs(int n) {
-            int f1 = 1;
-            int f2 = 1;
-            for (int i = 2; i <= n; i++) {
-                int temp = f1 + f2;
-                f1 = f2;
-                f2 = temp;
-            }
-            return f2;
-        }
+  /*
+      Climbing Stairs
+      Leetcode #70
+      https://leetcode.com/problems/climbing-stairs/
+      Difficulty: Easy
+   */
+  public class Solution {
+    public int climbStairs(int n) {
+      int f1 = 1;
+      int f2 = 1;
+      for (int i = 2; i <= n; i++) {
+        int temp = f1 + f2;
+        f1 = f2;
+        f2 = temp;
+      }
+      return f2;
+    }
+  }
+
+  /*
+      Climbing Stairs
+      Leetcode #70
+      https://leetcode.com/problems/climbing-stairs/
+      Difficulty: Easy
+   */
+  public class Solution_2 {
+    public int climbStairs(int n) {
+      double root5 = Math.sqrt(5);
+//      return (int) ((Math.pow((1 + root5) / 2, n + 1) - Math.pow((1 - root5) / 2, n + 1)) / root5 + 0.5);
+      double phi = (1 + root5) / 2;
+      return (int) (Math.pow(phi, n + 1) / root5 + 0.5);
+    }
+  }
+
+  public static class UnitTest {
+    @Test
+    public void test1() {
+      Solution sol = new ClimbingStairs().new Solution();
+      assertEquals(1, sol.climbStairs(1));
+      assertEquals(2, sol.climbStairs(2));
+      assertEquals(3, sol.climbStairs(3));
+      assertEquals(5, sol.climbStairs(4));
+      assertEquals(8, sol.climbStairs(5));
+      assertEquals(13, sol.climbStairs(6));
+      assertEquals(21, sol.climbStairs(7));
     }
 
-    /*
-        Climbing Stairs
-        Leetcode #70
-        https://leetcode.com/problems/climbing-stairs/
-        Difficulty: Easy
-     */
-    public class Solution_2 {
-        public int climbStairs(int n) {
-            double s = Math.sqrt(5);
-            return (int) ((Math.pow((1 + s) / 2, n + 1) - Math.pow((1 - s) / 2, n + 1)) / s + 0.5);
-        }
+    @Test
+    public void test2() {
+      Solution_2 sol = new ClimbingStairs().new Solution_2();
+      assertEquals(1, sol.climbStairs(1));
+      assertEquals(2, sol.climbStairs(2));
+      assertEquals(3, sol.climbStairs(3));
+      assertEquals(5, sol.climbStairs(4));
+      assertEquals(8, sol.climbStairs(5));
+      assertEquals(13, sol.climbStairs(6));
+      assertEquals(21, sol.climbStairs(7));
     }
-
-    public static class UnitTest {
-        @Test
-        public void test1() {
-            Solution sol = new ClimbingStairs().new Solution();
-            assertTrue(true);
-        }
-    }
+  }
 }
