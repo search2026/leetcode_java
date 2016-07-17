7c9e23bab1176514a6e675b640206b154ca76da5
Daniel <search2018@gmail.com>
2016-07-17 13:22:36 -0700

    add leetcode 375

--- src/main/java/guess_number_higher_or_lower/GuessNumberHigherorLower.java
+++ src/main/java/guess_number_higher_or_lower/GuessNumberHigherorLower.java
@@ -51,13 +51,89 @@ public class GuessNumberHigherorLower {
     }
   }
 
+  /*
+      Guess Number Higher or Lower II
+      Leetcode #375
+      https://leetcode.com/problems/guess-number-higher-or-lower-ii/
+      Difficulty: Medium
+   */
+  public class Solution_2 {
+    public int getMoneyAmount(int n) {
+      if (n == 1) return 0;
+
+      int[][] dp = new int[n + 1][n + 1];
+      // the calculations of longer intervals rely on shorter ones
+      for (int l = 2; l <= n; l++) {
+        // iterate all the intervals with length l, the start of which is i.
+        // Hence the interval will be [i, i + (l - 1)]
+        for (int i = 1; i <= n - (l - 1); i++) {
+          dp[i][i + (l - 1)] = Integer.MAX_VALUE;
+          // iterate all the first guesses g
+          for (int g = i; g <= i + (l - 1); g++) {
+            int currMinCost;
+            // since if g is the last integer, g + 1 does not exist, we have to separate this case
+            // cost for [i, i + (l - 1)]: g (first guess) + max{the cost of left part [i, g - 1],
+            // the cost of right part [g + 1, i + (l - 1)]}
+            if (g == n) {
+              currMinCost = dp[i][g - 1] + g;
+            } else {
+              currMinCost = g + Math.max(dp[i][g - 1], dp[g + 1][i + (l - 1)]);
+            }
+            dp[i][i + (l - 1)] = Math.min(dp[i][i + (l - 1)], currMinCost);
+          }
+        }
+      }
+      return dp[1][n];
+    }
+  }
+
+  /*
+      Guess Number Higher or Lower II
+      Leetcode #375
+      https://leetcode.com/problems/guess-number-higher-or-lower-ii/
+      Difficulty: Medium
+   */
+  public class Solution_3 {
+    public int getMoneyAmount(int n) {
+      if (n == 1) return 0;
+      int[][] dp = new int[n + 2][n + 2];
+      for (int i = 1; i <= n; ++i) {
+        for (int j = i - 1; j >= 1; --j) {
+          int currMinCost = Integer.MAX_VALUE;
+          for (int k = j; k <= i; ++k) {
+            int tmp = k + Math.max(dp[j][k - 1], dp[k + 1][i]);
+            currMinCost = Math.min(currMinCost, tmp);
+          }
+          dp[j][i] = currMinCost;
+        }
+      }
+      return dp[1][n];
+    }
+  }
+
   public static class UnitTest {
     @Test
     public void test1() {
       Solution sol = new GuessNumberHigherorLower().new Solution(6);
       assertEquals(6, sol.guessNumber(10));
-      assertEquals(1, sol.guessNumber(5));
+      assertEquals(6, sol.guessNumber(5));
       assertEquals(6, sol.guessNumber(100));
+    }
+
+    @Test
+    public void test2() {
+      Solution_2 sol = new GuessNumberHigherorLower().new Solution_2();
+      assertEquals(16, sol.getMoneyAmount(10));
+      assertEquals(6, sol.getMoneyAmount(5));
+      assertEquals(400, sol.getMoneyAmount(100));
+    }
+
+    @Test
+    public void test3() {
+      Solution_3 sol = new GuessNumberHigherorLower().new Solution_3();
+      assertEquals(16, sol.getMoneyAmount(10));
+      assertEquals(6, sol.getMoneyAmount(5));
+      assertEquals(400, sol.getMoneyAmount(100));
     }
   }
 }
