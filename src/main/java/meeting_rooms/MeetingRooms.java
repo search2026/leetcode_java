526e28a97a04aea7a61a0c4e61a28ecd01422ea0
Daniel Q <search2018@gmail.com>
2017-11-02 09:08:10 -0700

    update of 10/31/2017

--- src/main/java/meeting_rooms/MeetingRooms.java
+++ src/main/java/meeting_rooms/MeetingRooms.java
@@ -3,9 +3,7 @@ package meeting_rooms;
 import common.Interval;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -131,6 +129,69 @@ public class MeetingRooms {
         }
     }
 
+    /*
+        Meeting Time
+        Find time intervals, which have no meeting
+        https://www.quora.com/What-is-the-algorithmic-approach-to-find-the-free-time-intervals-of-both-people-so-they-can-arrange-a-meeting-given-the-set-of-busy-time-intervals-of-two-people-as-in-a-calendar
+        Difficulty: Medium
+     */
+    public class Solution_5 {
+        class Point implements Comparable<Point> {
+            int time;
+            boolean isStart;
+            Point(int time, boolean isStart) {
+                this.time = time;
+                this.isStart = isStart;
+            }
+            @Override
+            public int compareTo(Point that) {
+                if (this.time != that.time || this.isStart == that.isStart) {
+                    return this.time - that.time;
+                } else {
+                    return this.isStart ? -1 : 1;
+                }
+            }
+        }
+
+        public List<Interval> getAvailableIntervals(List<List<Interval>> intervals, int k) {
+            List<Interval> res = new ArrayList<>();
+            List<Point> points = new ArrayList<>();
+
+            for (List<Interval> intervalList : intervals) {
+                for (Interval interval : intervalList) {
+                    points.add(new Point(interval.start, true));
+                    points.add(new Point(interval.end, false));
+                }
+            }
+            Collections.sort(points);
+
+            int count = 0;
+            Integer availableStart = null;
+            for (int i = 0; i < points.size(); i++) {
+                Point point = points.get(i);
+                if (point.isStart) {
+                    count++;
+                    if (availableStart == null && i == 0 && count <= intervals.size() - k) {
+                        availableStart = point.time;
+                    } else if (availableStart != null && count == intervals.size() - k + 1) {
+                        res.add(new Interval(availableStart, point.time));
+                        availableStart = null;
+                    }
+                } else {
+                    count--;
+                    if (count == intervals.size() - k && i < points.size() - 1) {
+                        availableStart = point.time;
+                    } else if (availableStart != null && i == points.size() - 1 && count <= intervals.size() - k) {
+                        res.add(new Interval(availableStart, point.time));
+                        availableStart = null;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+
     public static class UnitTest {
         @Test
         public void test1() {
@@ -174,6 +235,30 @@ public class MeetingRooms {
                     new Interval(15, 20)
             };
             assertEquals(2, sol.minMeetingRooms(intervals));
+        }
+
+        @Test
+        public void test5() {
+            Solution_5 sol = new MeetingRooms().new Solution_5();
+            List<List<Interval>> intervals = new ArrayList<List<Interval>>() {{
+               add(new ArrayList<Interval>() {{
+                   add(new Interval(1, 3));
+                   add(new Interval(6, 7));
+               }});
+                add(new ArrayList<Interval>() {{
+                    add(new Interval(2, 4));
+                }});
+                add(new ArrayList<Interval>() {{
+                    add(new Interval(2, 3));
+                    add(new Interval(9, 12));
+                }});
+            }};
+            List<Interval> res = sol.getAvailableIntervals(intervals, 3);
+            assertEquals(2, res.size());
+            assertEquals(4, res.get(0).start);
+            assertEquals(6, res.get(0).end);
+            assertEquals(7, res.get(1).start);
+            assertEquals(9, res.get(1).end);
         }
     }
 }
