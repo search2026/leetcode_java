4d33f3d23a329b37933a44a7b427b1b985fc2ac4
Daniel Q <search2018@gmail.com>
2017-11-07 08:58:18 -0800

    update of 11/7/2018

--- src/main/java/happy_number/HappyNumber.java
+++ src/main/java/happy_number/HappyNumber.java
@@ -3,8 +3,11 @@ package happy_number;
 import org.junit.Test;
 
 import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class HappyNumber {
     /*
@@ -14,39 +17,52 @@ public class HappyNumber {
         Difficulty: Easy
      */
     public class Solution {
-        public int[] getDigits(int n) {
-            String s = String.valueOf(n);
-            int[] result = new int[s.length()];
-            int i = 0;
-
-            while (n > 0) {
-                int m = n % 10;
-                result[i++] = m;
-                n = n / 10;
+        private int cal(int n) {
+            int x = n;
+            int s = 0;
+            while (x > 0) {
+                s = s + (x % 10) * (x % 10);
+                x = x / 10;
             }
-
-            return result;
-        }
-
-        public int sum(int[] arr) {
-            int sum = 0;
-            for (int i : arr) {
-                sum = sum + i * i;
-            }
-            return sum;
+            return s;
         }
 
         public boolean isHappy(int n) {
-            HashSet<Integer> set = new HashSet<Integer>();
-
-            while (!set.contains(n)) {
-                set.add(n);
-
-                n = sum(getDigits(n));
-                if (n == 1)
-                    return true;
+            int x = n;
+            int y = n;
+            while (x > 1) {
+                x = cal(x);
+                if (x == 1) return true;
+                y = cal(cal(y));
+                if (y == 1) return true;
+                if (x == y) return false;
             }
+            return true;
+        }
+    }
 
+    /*
+        Happy Number
+        leetcode #202
+        https://leetcode.com/problems/happy-number/
+        Difficulty: Easy
+     */
+    public class Solution_2 {
+        public boolean isHappy(int n) {
+            Set<Integer> inLoop = new HashSet<>();
+            int squareSum, remain;
+            while (inLoop.add(n)) {
+                squareSum = 0;
+                while (n > 0) {
+                    remain = n % 10;
+                    squareSum += remain * remain;
+                    n /= 10;
+                }
+                if (squareSum == 1)
+                    return true;
+                else
+                    n = squareSum;
+            }
             return false;
         }
     }
@@ -55,7 +71,15 @@ public class HappyNumber {
         @Test
         public void test1() {
             Solution sol = new HappyNumber().new Solution();
-            assertEquals(3, 3);
+            assertTrue(sol.isHappy(19));
+            assertFalse(sol.isHappy(18));
+        }
+
+        @Test
+        public void test2() {
+            Solution_2 sol = new HappyNumber().new Solution_2();
+            assertTrue(sol.isHappy(19));
+            assertFalse(sol.isHappy(18));
         }
     }
 }
