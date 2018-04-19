fad52c1c1521ba198e4eaa2d6e05a7b5f6f8d239
Daniel <daniel@celential.ai>
2018-04-18 23:03:20 -0700

    update of 4/18/2018

--- src/main/java/insert_interval/InsertInterval.java
+++ src/main/java/insert_interval/InsertInterval.java
@@ -9,117 +9,117 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InsertInterval {
-  /*
-      Insert Interval
-      Leetcode #57
-      https://leetcode.com/problems/insert-interval/
-      Difficulty: Hard
-   */
-  public class Solution {
-    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-      if (newInterval == null || intervals == null) return intervals;
+    /*
+        Insert Interval
+        Leetcode #57
+        https://leetcode.com/problems/insert-interval/
+        Difficulty: Hard
+     */
+    public class Solution {
+        public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+            if (newInterval == null || intervals == null) return intervals;
 
-      List<Interval> intervalList = new ArrayList<>();
-      int pos = 0;
+            List<Interval> intervalList = new ArrayList<>();
+            int pos = 0;
 
-      for (Interval interval : intervals) {
-        if (interval.end < newInterval.start) {
-          intervalList.add(interval);
-          pos++;
-        } else if (interval.start > newInterval.end) {
-          intervalList.add(interval);
-        } else {
-          newInterval.start = Math.min(interval.start, newInterval.start);
-          newInterval.end = Math.max(interval.end, newInterval.end);
+            for (Interval interval : intervals) {
+                if (interval.end < newInterval.start) {
+                    intervalList.add(interval);
+                    pos++;
+                } else if (interval.start > newInterval.end) {
+                    intervalList.add(interval);
+                } else {
+                    newInterval.start = Math.min(interval.start, newInterval.start);
+                    newInterval.end = Math.max(interval.end, newInterval.end);
+                }
+            }
+
+            intervalList.add(pos, newInterval);
+
+            return intervalList;
         }
-      }
-
-      intervalList.add(pos, newInterval);
-
-      return intervalList;
-    }
-  }
-
-  /*
-      Insert Interval - Using existed list
-      Leetcode #57
-      https://leetcode.com/problems/insert-interval/
-      Difficulty: Hard
-   */
-  public class Solution_2 {
-    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-      if (newInterval == null || intervals == null) return intervals;
-
-      int pos = 0;
-      while (pos < intervals.size() && intervals.get(pos).end < newInterval.start) pos++;
-      while (pos < intervals.size() && intervals.get(pos).start <= newInterval.end) {
-        newInterval.start = Math.min(intervals.get(pos).start, newInterval.start);
-        newInterval.end = Math.max(intervals.get(pos).end, newInterval.end);
-        intervals.remove(pos);
-      }
-      intervals.add(pos, newInterval);
-
-      return intervals;
-    }
-  }
-
-  public static class UnitTest {
-    @Test
-    public void test1() {
-      Solution sol = new InsertInterval().new Solution();
-      List<Interval> intervals = new ArrayList<>();
-      intervals.add(new Interval(1, 3));
-      intervals.add(new Interval(6, 9));
-      List<Interval> rslt = sol.insert(intervals, new Interval(2, 5));
-      assertEquals(2, rslt.size());
-      assertEquals(1, rslt.get(0).start);
-      assertEquals(5, rslt.get(0).end);
-      assertEquals(6, rslt.get(1).start);
-      assertEquals(9, rslt.get(1).end);
-
-      intervals = new ArrayList<>();
-      intervals.add(new Interval(1, 2));
-      intervals.add(new Interval(3, 5));
-      intervals.add(new Interval(6, 7));
-      intervals.add(new Interval(8, 10));
-      intervals.add(new Interval(12, 16));
-      rslt = sol.insert(intervals, new Interval(4, 9));
-      assertEquals(3, rslt.size());
-      assertEquals(1, rslt.get(0).start);
-      assertEquals(2, rslt.get(0).end);
-      assertEquals(3, rslt.get(1).start);
-      assertEquals(10, rslt.get(1).end);
-      assertEquals(12, rslt.get(2).start);
-      assertEquals(16, rslt.get(2).end);
     }
 
-    @Test
-    public void test2() {
-      Solution_2 sol = new InsertInterval().new Solution_2();
-      List<Interval> intervals = new ArrayList<>();
-      intervals.add(new Interval(1, 3));
-      intervals.add(new Interval(6, 9));
-      List<Interval> rslt = sol.insert(intervals, new Interval(2, 5));
-      assertEquals(2, rslt.size());
-      assertEquals(1, rslt.get(0).start);
-      assertEquals(5, rslt.get(0).end);
-      assertEquals(6, rslt.get(1).start);
-      assertEquals(9, rslt.get(1).end);
+    /*
+        Insert Interval - Using existed list
+        Leetcode #57
+        https://leetcode.com/problems/insert-interval/
+        Difficulty: Hard
+     */
+    public class Solution_2 {
+        public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+            if (newInterval == null || intervals == null) return intervals;
 
-      intervals = new ArrayList<>();
-      intervals.add(new Interval(1, 2));
-      intervals.add(new Interval(3, 5));
-      intervals.add(new Interval(6, 7));
-      intervals.add(new Interval(8, 10));
-      intervals.add(new Interval(12, 16));
-      rslt = sol.insert(intervals, new Interval(4, 9));
-      assertEquals(3, rslt.size());
-      assertEquals(1, rslt.get(0).start);
-      assertEquals(2, rslt.get(0).end);
-      assertEquals(3, rslt.get(1).start);
-      assertEquals(10, rslt.get(1).end);
-      assertEquals(12, rslt.get(2).start);
-      assertEquals(16, rslt.get(2).end);
+            int pos = 0;
+            while (pos < intervals.size() && intervals.get(pos).end < newInterval.start) pos++;
+            while (pos < intervals.size() && intervals.get(pos).start <= newInterval.end) {
+                newInterval.start = Math.min(intervals.get(pos).start, newInterval.start);
+                newInterval.end = Math.max(intervals.get(pos).end, newInterval.end);
+                intervals.remove(pos);
+            }
+            intervals.add(pos, newInterval);
+
+            return intervals;
+        }
     }
-  }
+
+    public static class UnitTest {
+        @Test
+        public void test1() {
+            Solution sol = new InsertInterval().new Solution();
+            List<Interval> intervals = new ArrayList<>();
+            intervals.add(new Interval(1, 3));
+            intervals.add(new Interval(6, 9));
+            List<Interval> rslt = sol.insert(intervals, new Interval(2, 5));
+            assertEquals(2, rslt.size());
+            assertEquals(1, rslt.get(0).start);
+            assertEquals(5, rslt.get(0).end);
+            assertEquals(6, rslt.get(1).start);
+            assertEquals(9, rslt.get(1).end);
+
+            intervals = new ArrayList<>();
+            intervals.add(new Interval(1, 2));
+            intervals.add(new Interval(3, 5));
+            intervals.add(new Interval(6, 7));
+            intervals.add(new Interval(8, 10));
+            intervals.add(new Interval(12, 16));
+            rslt = sol.insert(intervals, new Interval(4, 9));
+            assertEquals(3, rslt.size());
+            assertEquals(1, rslt.get(0).start);
+            assertEquals(2, rslt.get(0).end);
+            assertEquals(3, rslt.get(1).start);
+            assertEquals(10, rslt.get(1).end);
+            assertEquals(12, rslt.get(2).start);
+            assertEquals(16, rslt.get(2).end);
+        }
+
+        @Test
+        public void test2() {
+            Solution_2 sol = new InsertInterval().new Solution_2();
+            List<Interval> intervals = new ArrayList<>();
+            intervals.add(new Interval(1, 3));
+            intervals.add(new Interval(6, 9));
+            List<Interval> rslt = sol.insert(intervals, new Interval(2, 5));
+            assertEquals(2, rslt.size());
+            assertEquals(1, rslt.get(0).start);
+            assertEquals(5, rslt.get(0).end);
+            assertEquals(6, rslt.get(1).start);
+            assertEquals(9, rslt.get(1).end);
+
+            intervals = new ArrayList<>();
+            intervals.add(new Interval(1, 2));
+            intervals.add(new Interval(3, 5));
+            intervals.add(new Interval(6, 7));
+            intervals.add(new Interval(8, 10));
+            intervals.add(new Interval(12, 16));
+            rslt = sol.insert(intervals, new Interval(4, 9));
+            assertEquals(3, rslt.size());
+            assertEquals(1, rslt.get(0).start);
+            assertEquals(2, rslt.get(0).end);
+            assertEquals(3, rslt.get(1).start);
+            assertEquals(10, rslt.get(1).end);
+            assertEquals(12, rslt.get(2).start);
+            assertEquals(16, rslt.get(2).end);
+        }
+    }
 }
--- src/main/java/merge_intervals/MergeIntervals.java
+++ src/main/java/merge_intervals/MergeIntervals.java
@@ -8,100 +8,100 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MergeIntervals {
-  /*
-      Merge Intervals
-      Leetcode #56
-      https://leetcode.com/problems/merge-intervals/
-      Difficulty: Hard
-   */
-  public class Solution {
-    private class IntervalComparator implements Comparator<Interval> {
-      @Override
-      public int compare(Interval a, Interval b) {
-        return a.start - b.start;
-      }
-    }
-
-    public List<Interval> merge(List<Interval> intervals) {
-      if (intervals == null || intervals.size() == 0) return intervals;
-      
-      Collections.sort(intervals, (Interval i1, Interval i2) -> (i1.start - i2.start));
-
-      List<Interval> res = new ArrayList<>();
-      Interval prev = intervals.get(0);
-      for (int i = 1; i < intervals.size(); i++) {
-        Interval cur = intervals.get(i);
-        if (cur.start <= prev.end) {
-          prev.end = Math.max(prev.end, cur.end);
-        } else {
-          res.add(prev);
-          prev = cur;
+    /*
+        Merge Intervals
+        Leetcode #56
+        https://leetcode.com/problems/merge-intervals/
+        Difficulty: Hard
+     */
+    public class Solution {
+        private class IntervalComparator implements Comparator<Interval> {
+            @Override
+            public int compare(Interval a, Interval b) {
+                return a.start - b.start;
+            }
         }
-      }
 
-      res.add(prev);
-      return res;
-    }
-  }
+        public List<Interval> merge(List<Interval> intervals) {
+            if (intervals == null || intervals.size() == 0) return intervals;
 
-  /*
-      Merge Intervals - Using existed list
-      Leetcode #56
-      https://leetcode.com/problems/merge-intervals/
-      Difficulty: Hard
-   */
-  public class Solution_2 {
-    public List<Interval> merge(List<Interval> intervals) {
-      if (intervals == null || intervals.size() == 0) return intervals;
+            Collections.sort(intervals, (Interval i1, Interval i2) -> (i1.start - i2.start));
 
-      Collections.sort(intervals, (Interval i1, Interval i2) -> (i1.start - i2.start));
+            List<Interval> res = new ArrayList<>();
+            Interval prev = intervals.get(0);
+            for (int i = 1; i < intervals.size(); i++) {
+                Interval cur = intervals.get(i);
+                if (cur.start <= prev.end) {
+                    prev.end = Math.max(prev.end, cur.end);
+                } else {
+                    res.add(prev);
+                    prev = cur;
+                }
+            }
 
-      ListIterator<Interval> it = intervals.listIterator();
-      Interval prev = it.next();
-      while (it.hasNext()) {
-        Interval curr = it.next();
-        if (curr.start <= prev.end) {
-          prev.end = Math.max(prev.end, curr.end);
-          it.remove();
-        } else {
-          prev = curr;
+            res.add(prev);
+            return res;
         }
-      }
-      return intervals;
-    }
-  }
-
-  public static class UnitTest {
-    @Test
-    public void test1() {
-      Solution sol = new MergeIntervals().new Solution();
-      List<Interval> intervals = new ArrayList<>();
-      intervals.add(new Interval(1, 3));
-      intervals.add(new Interval(2, 6));
-      intervals.add(new Interval(8, 10));
-      intervals.add(new Interval(15, 18));
-      List<Interval> rslt = sol.merge(intervals);
-      assertEquals(3, rslt.size());
-      assertEquals(1, rslt.get(0).start);
-      assertEquals(6, rslt.get(0).end);
-      assertEquals(8, rslt.get(1).start);
-      assertEquals(10, rslt.get(1).end);
     }
 
-    @Test
-    public void test2() {
-      Solution_2 sol = new MergeIntervals().new Solution_2();
-      List<Interval> intervals = new ArrayList<>();
-      intervals.add(new Interval(1, 3));
-      intervals.add(new Interval(2, 6));
-      intervals.add(new Interval(8, 10));
-      intervals.add(new Interval(15, 18));
-      List<Interval> rslt = sol.merge(intervals);
-      assertEquals(3, rslt.size());
-      assertEquals(1, rslt.get(0).start);
-      assertEquals(6, rslt.get(0).end);
-      assertEquals(8, rslt.get(1).start);
-      assertEquals(10, rslt.get(1).end);
+    /*
+        Merge Intervals - Using existed list
+        Leetcode #56
+        https://leetcode.com/problems/merge-intervals/
+        Difficulty: Hard
+     */
+    public class Solution_2 {
+        public List<Interval> merge(List<Interval> intervals) {
+            if (intervals == null || intervals.size() == 0) return intervals;
+
+            Collections.sort(intervals, (Interval i1, Interval i2) -> (i1.start - i2.start));
+
+            ListIterator<Interval> it = intervals.listIterator();
+            Interval prev = it.next();
+            while (it.hasNext()) {
+                Interval curr = it.next();
+                if (curr.start <= prev.end) {
+                    prev.end = Math.max(prev.end, curr.end);
+                    it.remove();
+                } else {
+                    prev = curr;
+                }
+            }
+            return intervals;
+        }
     }
-  }
+
+    public static class UnitTest {
+        @Test
+        public void test1() {
+            Solution sol = new MergeIntervals().new Solution();
+            List<Interval> intervals = new ArrayList<>();
+            intervals.add(new Interval(1, 3));
+            intervals.add(new Interval(2, 6));
+            intervals.add(new Interval(8, 10));
+            intervals.add(new Interval(15, 18));
+            List<Interval> rslt = sol.merge(intervals);
+            assertEquals(3, rslt.size());
+            assertEquals(1, rslt.get(0).start);
+            assertEquals(6, rslt.get(0).end);
+            assertEquals(8, rslt.get(1).start);
+            assertEquals(10, rslt.get(1).end);
+        }
+
+        @Test
+        public void test2() {
+            Solution_2 sol = new MergeIntervals().new Solution_2();
+            List<Interval> intervals = new ArrayList<>();
+            intervals.add(new Interval(1, 3));
+            intervals.add(new Interval(2, 6));
+            intervals.add(new Interval(8, 10));
+            intervals.add(new Interval(15, 18));
+            List<Interval> rslt = sol.merge(intervals);
+            assertEquals(3, rslt.size());
+            assertEquals(1, rslt.get(0).start);
+            assertEquals(6, rslt.get(0).end);
+            assertEquals(8, rslt.get(1).start);
+            assertEquals(10, rslt.get(1).end);
+        }
+    }
 }
